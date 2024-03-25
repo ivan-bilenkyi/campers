d595a0975cda65263f9f1940fb2da05682e0bb92
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2024-03-25 17:33:03 +0200

    fix bugs github pages

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -2,17 +2,19 @@ import {useSelector} from "react-redux";
 import {selectorFavorite} from "../redux/favorite/selectors.js";
 import { List, ListWrapp, Wrapper} from "../components/CamperList/CamperList.styled.js";
 import {CamperItem} from "../components/CamperItem/CamperItem.jsx";
+import { Section } from '../components/Container.styled.js';
 
 export default function Favorite() {
     const campers = useSelector(selectorFavorite)
     return (
-        <Wrapper>
-            <ListWrapp>
-                <List>
-                    {campers && campers.length > 0 && campers.map(item => <CamperItem key={item._id} camper={item} />)}
-                </List>
-            </ListWrapp>
-        </Wrapper>
-
+        <Section>
+            <Wrapper>
+                <ListWrapp>
+                    <List>
+                        {campers && campers.length > 0 && campers.map(item => <CamperItem key={item._id} camper={item} />)}
+                    </List>
+                </ListWrapp>
+            </Wrapper>
+        </Section>
     );
 }
